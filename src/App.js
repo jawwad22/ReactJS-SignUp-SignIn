fe44a0311a59b1fd37e4b30de382dbe50bb87838
unknown <mjawwadiqbal22@gmail.com>
2019-01-13 09:41:25 -0800

    --Commit:Buttom Added on form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import styles from './App.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='App__Aside'></div>
+        <div className='App__Form'>
+          <div className="PageSwitcher">
+            <a href="#" className="PageSwitcher__Item">Sign In</a>
+            <a href="#" className="PageSwitcher__Item PageSwitcher__Item--Active">Sign Up</a>
+          </div>
+        </div>
 
       </div>
     );
